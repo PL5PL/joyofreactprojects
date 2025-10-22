2ade14bc54a0c2c242a90addc905ec6d3aa1c759
Lance Park <parklance@outlook.com>
2025-10-22 23:59:00 +0800

    Exercise 1: GuessInput Complete

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,35 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        
+        /* Log the entered value */
+        console.info( {guess})
+        /* Resets the input */
+        setGuess('');
+      }}
+    >
+      <label htmlFor="guess-input">Enter Guess</label>
+      <input 
+        id="guess-input" 
+        type="text"
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value.toUpperCase())
+        }}
+
+        /* from Solution */ 
+        pattern="[a-zA-Z]{5}"
+        title="5 Letter Word"
+        />
+    </form>
+  );
 }
 
 export default Game;
